7245260aaf90a1c5716ac0a259d7c1912679a5d7
ziggyzulu <k00083370@student.lit.ie>
2019-11-19 15:31:13 +0000

    Filter working

--- hh-technical/src/app/job-listing/job-listing.component.ts
+++ hh-technical/src/app/job-listing/job-listing.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { JobsApiService } from '../jobs-api.service';
-import { MatTableDataSource, MatSort, MatPaginator, MatTable } from '@angular/material';
+import { MatTableDataSource, MatSort, MatPaginator, MatTable} from '@angular/material';
 
 @Component({
   selector: 'app-job-listing',
@@ -20,15 +20,24 @@ export class JobListingComponent implements OnInit {
 
   ngOnInit() {
 
-    // Retrieve jobs list and assign to jobsList variable
+    // Retrieve jobs list from the API service and assign to jobsList variable
 
     this.apiService.getJobListing().subscribe((data: any[]) => {
 
       this.jobsList = new MatTableDataSource(data['jobs']);
 
+      // Setting a custom filter to determine which columns to search by.
+      // This will ignore everything besides the title and description.text
+
+      this.jobsList.filterPredicate = (data, filter) => {
+        const dataStr = data.title.toLowerCase() + data.description.text.toLowerCase();
+        return dataStr.indexOf(filter) !== -1;
+      };
+
       // Once data is returned and assigned, populate the table
 
       this.populateData();
+
     });
 
   }
@@ -37,6 +46,24 @@ export class JobListingComponent implements OnInit {
 
     this.table.dataSource = this.jobsList;
 
+
+  }
+
+  applyFilter() {
+
+    // This applies the filter to the job list data.
+
+    this.jobsList.filter = this.searchKey.trim();
+
+  }
+
+
+
+  // Reset the search term to an empty string.
+  clearSearch() {
+
+    this.searchKey = '';
+    this.applyFilter();
 
   }
 
