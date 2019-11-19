9b0d6d8b96d700a813f214eb1afc21b1e7504fcb
ziggyzulu <k00083370@student.lit.ie>
2019-11-19 13:09:41 +0000

    created route to job listing

--- hh-technical/src/app/app-routing.module.ts
+++ hh-technical/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {JobListingComponent} from './job-listing/job-listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: JobListingComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
